f8fa603c3562e432c39f7d03920e466229fe7a94
xiaofei.yan <xiaofei.yan@tongdun.cn>
2020-10-12 12:15:30 +0800

    feat: 增加站点A

---
+++ wn/src/main/java/com/isyxf/sso/wn/WnApplication.java
@@ -0,0 +1,11 @@
+package com.isyxf.sso.wn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WnApplication.class, args);
+    }
+}
