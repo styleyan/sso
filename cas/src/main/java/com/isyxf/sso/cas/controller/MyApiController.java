d96649169423b43d912f368c38c1a13931ba3fcc
xiaofei.yan <xiaofei.yan@tongdun.cn>
2020-10-14 18:47:18 +0800

    feat: 增加登录状态

--- cas/src/main/java/com/isyxf/sso/cas/controller/MyApiController.java
+++ cas/src/main/java/com/isyxf/sso/cas/controller/MyApiController.java
@@ -17,6 +17,9 @@ import java.util.UUID;
 
 @RestController
 public class MyApiController extends BaseController {
+    private static String REDIS_USER_TOKEN = "REDIS_USER_TOKEN";
+    private static String REDIS_USER_TICKET = "REDIS_USER_TICKET";
+
     @Autowired
     private LoginService loginService;
 
@@ -25,16 +28,18 @@ public class MyApiController extends BaseController {
         User user = loginService.queryUser(userBO);
 
         if (user != null) {
-            String ticket = "userToken:" + UUID.randomUUID().toString().trim() + user.getId();
-            int timeout = 60 * 60 *24 *365;
-            RedisUtils.setValueTimeout(ticket, JSON.toJSONString(user), timeout);
-            Cookie cookie = new Cookie("access_token", ticket);
-            cookie.setHttpOnly(true);
-            cookie.setPath("/");
-            cookie.setMaxAge(timeout);
+            // 2. 实现 redis 会话
+            String uniqueToken = UUID.randomUUID().toString().trim();
+            user.setUserUniqueToken(uniqueToken);
+            RedisUtils.setValue(REDIS_USER_TOKEN + ":" + user.getId(), JSON.toJSONString(user));
 
-            response.addCookie(cookie);
-            user.setReturnUrl(userBO.getReturnUrl() + "?=ticket=" + "fffffff");
+            // 3. 生成 ticket 门票，全局门票，代表用户在CAS端登录过
+            String userTicket = UUID.randomUUID().toString().trim();
+
+            // 4. userTicket 关联用户id, 并且放入到redis中，代表这个用户有门票
+            RedisUtils.setValue(REDIS_USER_TICKET + ":" + userTicket, user.getId());
+
+            // 5. 生成临时票据，回跳到调用端网站，是有CAS端所签发的一个临时性ticket
             return JsonResult.success(user);
         } else {
             return JsonResult.failure(2001, "用户不存在");
--- cas/src/main/java/com/isyxf/sso/cas/pojo/User.java
+++ cas/src/main/java/com/isyxf/sso/cas/pojo/User.java
@@ -77,4 +77,9 @@ public class User {
      * 回调地址
      */
     private String returnUrl;
+
+    /**
+     * token
+     */
+    private String userUniqueToken;
 }
